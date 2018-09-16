ec1d79d5e9da9a56b4f4caa8ae1a1ccfe50e3d69
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-09-16 17:03:03 -0300

    product grid

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$("span.menu").click(function(){
+								$(".top-menu ul").slideToggle("slow" , function(){
+								});
+							});
+
+
+
+$(function() {
+			$('.moments-bottom a').Chocolat();
+		});
